1fd21bd08fd299c48153358b8e8a0741a4b4f7a0
hoangxuanthang7622 <hoangxuanthang7622@gmail.com>
2022-08-09 14:20:56 +0700

    baita3

---
+++ baitap/bai14/bai3.php
@@ -0,0 +1,10 @@
+<?php
+    $Pattern = '/^[CAP][0-9]{4,4}[GHIKLM]$/';
+    $string = 'P0323A';
+    if(preg_match($Pattern,$string)){
+        echo 'tên lớp hợp lệ ' . $string;
+    }else{
+        echo 'tên lớp không hợp lệ ' . $string;
+
+    }
+    

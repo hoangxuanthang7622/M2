3a15fd8ab263b38aee8d43b3be73b619110d1db7
hoangxuanthang7622 <hoangxuanthang7622@gmail.com>
2022-08-09 14:08:22 +0700

    baitap2

--- baitap/bai14/bai2.php
+++ baitap/bai14/bai2.php
@@ -1,2 +1,9 @@
 <?php
-        
+    $Pattern = '/^[_a-z0-9]{6,}$/';
+    $a = '______';
+    if (preg_match($Pattern,$a)){
+        echo 'mẫu account hợp lệ'; //
+    }else{
+        echo 'mẫu account không hợp lệ';
+
+    }
